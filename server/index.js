fff56f13b86068d5f37f8fc642ab69d614998a8d
bexy65 <naimertan65@gmail.com>
2024-05-05 00:46:39 +0300

    create basic setup for backend using express

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+require("dotenv").config();
+const User = require("./db/user");
 
 const app = express();
+const userController = new User();
 
 app.get("/", (req, res) => {
   res.send(html);
   console.log("Sended");
 });
-
+userController.createUser("test@gmail.com", "12341234", "boba");
 app.listen(3000, () => {
   console.log("Listening on port 3000\n");
 });
