4e3c808f5413884ee388cd96531ce6add0409998
bexy65 <naimertan65@gmail.com>
2024-05-05 23:22:29 +0300

    helper function for hashing the pass for login

---
+++ server/utilities/util.js
@@ -0,0 +1,14 @@
+const bcrypt = require("bcrypt");
+
+async function hashPassword(password) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    throw error;
+  }
+}
+
+module.exports = hashPassword;
