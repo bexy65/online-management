ddbc762fb630da0e82511574e9e37f129effdb5d
bexy65 <naimertan65@gmail.com>
2024-05-05 23:20:08 +0300

    create login controller with jwt

---
+++ server/controller/login.js
@@ -0,0 +1,25 @@
+const userDb = require("../db/user");
+const jwt = require("jsonwebtoken");
+const User = new userDb();
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  try {
+    const user = await User.getUser({ email });
+    const isAuth = User.authUser({ email, password });
+
+    if (!user || !isAuth) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "secret", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
+module.exports = login;
