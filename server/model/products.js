96de3f0e52666f2f8ed6ff45e1c35e6cf47cde76
bexy65 <naimertan65@gmail.com>
2024-05-06 17:34:35 +0300

    change relation to be string instead to be object.id

--- server/model/products.js
+++ server/model/products.js
@@ -11,7 +11,7 @@ const productSchema = new mongoose.Schema({
     required: true,
   },
   category: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: "Category",
   },
   imageUrl: String,
