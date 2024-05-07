3cf26893e86e8bc3602f7a8136a32e497e16ac39
bexy65 <naimertan65@gmail.com>
2024-05-07 14:43:43 +0300

    on login context passed credential fields handlers

--- client/src/components/login/loginContext.jsx
+++ client/src/components/login/loginContext.jsx
@@ -6,9 +6,35 @@ const LoginContext = createContext();
 // Create a provider component
 const LoginContextProvider = ({ children }) => {
   const [isRegister, setIsRegister] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const updateUsername = (value) => {
+    setUsername(value);
+  };
+
+  const updatePassword = (value) => {
+    setPassword(value);
+  };
+
+  const updateEmail = (value) => {
+    setEmail(value);
+  };
 
   return (
-    <LoginContext.Provider value={{ isRegister, setIsRegister }}>
+    <LoginContext.Provider
+      value={{
+        isRegister,
+        setIsRegister,
+        username,
+        password,
+        email,
+        updateUsername,
+        updatePassword,
+        updateEmail,
+      }}
+    >
       {children}
     </LoginContext.Provider>
   );
