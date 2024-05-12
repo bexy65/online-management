1480966b4da5683c3bfc5f8e30484d6e93bee55e
bexy65 <naimertan65@gmail.com>
2024-05-13 01:10:16 +0300

    made router basic and logic for one protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,81 +1,26 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginForm from "./components/login/loginForm";
 import Home from "./home/Home";
-import {
-  LoginContextProvider,
-  LoginContext,
-} from "./components/login/loginContext";
-import { useContext, useEffect } from "react";
-import { jwtDecode } from "jwt-decode";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+import Login from "./login/login";
+import { LoginContextProvider } from "./components/login/loginContext";
+
+// import { jwtDecode } from "jwt-decode";
 
 function App() {
   return (
     <>
       <LoginContextProvider>
-        <AppRoutes />
+        <Router>
+          <Routes>
+            <Route element={<ProtectedRoutes />}>
+              <Route element={<Home />} path="/" exact />
+            </Route>
+            <Route element={<Login />} path="/login" />
+          </Routes>
+        </Router>
       </LoginContextProvider>
     </>
-  );
-}
-
-// const checkTokenExpiry = () => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     setIsAuth(true);
-//     const decodedToken = jwtDecode(token);
-//     console.log(decodedToken);
-//     const currentTime = Date.now() / 1000;
-//     if (decodedToken.exp < currentTime) {
-//       localStorage.removeItem("token");
-//       setIsAuth(false);
-//     }
-//   }
-// };
-
-// const intervalId = setInterval(checkTokenExpiry, 60000);
-
-// return () => clearInterval(intervalId);
-function AppRoutes() {
-  const { isAuth, setIsAuth } = useContext(LoginContext);
-
-  useEffect(() => {
-    const checkAuthStatus = () => {
-      const token = localStorage.getItem("token");
-      if (token && !isAuth) {
-        setIsAuth(true);
-        console.log(isAuth);
-      } else if (!token && isAuth) {
-        setIsAuth(false);
-      }
-    };
-
-    checkAuthStatus();
-
-    const intervalId = setInterval(checkAuthStatus, 5000);
-
-    return () => clearInterval(intervalId);
-  }, [setIsAuth, isAuth]);
-
-  return (
-    <Router>
-      <Routes>
-        {isAuth ? (
-          <Route path="/" element={<Home />} />
-        ) : (
-          <Route path="/login" element={<LoginForm />} />
-        )}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-}
-
-function NotFound() {
-  return (
-    <div>
-      <h1>404 - Not Found</h1>
-    </div>
   );
 }
 
