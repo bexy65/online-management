2b42e3db06f8f67fe0c5d9c9917325fd9a6dbd75
bexy65 <naimertan65@gmail.com>
2024-05-13 15:21:16 +0300

    placeholder page

---
+++ client/src/contact/contact.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <div>
+      <div>
+        <Button variant="contained">Logout</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
