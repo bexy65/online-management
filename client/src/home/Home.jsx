edca519429d7ee63697da32f9e488e71b2e83e14
bexy65 <naimertan65@gmail.com>
2024-05-13 15:22:47 +0300

    add logout button to test

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,5 +1,27 @@
+import { Button } from "@mui/material";
+import { LoginContext } from "../components/login/loginContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Welcome ot home!</div>;
+  const { setIsAuth } = useContext(LoginContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setIsAuth(false);
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button variant="contained" onClick={logout}>
+          Logout
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
