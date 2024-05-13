db51238973e69320cb942b7c9eb5760de110b70a
bexy65 <naimertan65@gmail.com>
2024-05-13 15:29:43 +0300

    on context making if user isAuth or not depending of token from server

--- client/src/components/login/loginContext.jsx
+++ client/src/components/login/loginContext.jsx
@@ -1,15 +1,25 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
-// Create a context object
 const LoginContext = createContext();
 
-// Create a provider component
 const LoginContextProvider = ({ children }) => {
   const [isRegister, setIsRegister] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(() => {
+    const storedToken = localStorage.getItem("token");
+    return storedToken ? true : null;
+  });
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsAuth(true);
+    } else {
+      setIsAuth(false);
+    }
+  }, []);
 
   const updateUsername = (value) => {
     setUsername(value);
