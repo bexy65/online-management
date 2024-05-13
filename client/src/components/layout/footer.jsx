2dca973a8928e51796d6d6b03c5f99ab8b7191fc
bexy65 <naimertan65@gmail.com>
2024-05-13 22:49:33 +0300

    basic footer for layout

---
+++ client/src/components/layout/footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex flex-row justify-around content-center align-middle top-0 left-0 mt-4 bg-gray-300">
+      <div className="flex flex-col">
+        <li>1</li>
+        <li>3</li>
+        <li>5</li>
+      </div>
+      <div>
+        <li>1</li>
+        <li>3</li>
+        <li>5</li>
+      </div>
+      <div>
+        <li>1</li>
+        <li>3</li>
+        <li>5</li>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
