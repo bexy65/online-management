d28785488b03e03bbbcf7c267370e0e7f440935c
bexy65 <naimertan65@gmail.com>
2024-05-13 22:50:23 +0300

    dummy list items component

---
+++ client/src/components/layout/listItems.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ListItems = () => {
+  return (
+    <ul className="flex flex-col w-auto">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </ul>
+  );
+};
+
+export default ListItems;
