14222e9233c16c3ead4bd4a2ba578c7bb4fbf8a1
bexy65 <naimertan65@gmail.com>
2024-05-15 23:51:06 +0300

    refactor and add basic checkts depending on the roles for the models to save

--- server/controller/register.js
+++ server/controller/register.js
@@ -2,14 +2,29 @@ const User = require("../db/user");
 const user = new User();
 
 async function register(req, res) {
-  const { username, email, password } = req.body;
+  const { username, email, password, role, grade, subject } = req.body;
   try {
     const isExists = await user.getUser(email);
-    if (!isExists) {
-      await user.createUser(email, password, username);
-      return res.status(201).json({ message: "User created succesfully!" });
+
+    if (isExists) {
+      return res.status(400).json({ message: "Email exists or invalid" });
     }
-    return res.status(400).json({ message: "Email exists or invalid" });
+
+    if (!role || (role !== "teacher" && role !== "student")) {
+      return res.status(400).json({ message: "Invalid role" });
+    }
+
+    if (role === "teacher") {
+      await user.createTeacher(email, password, username, subject);
+    }
+
+    if (role === "student") {
+      await user.createStudent(email, password, username, grade);
+    }
+
+    return res
+      .status(200)
+      .json({ message: `${username} created succesfully!` });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
