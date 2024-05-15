c30a0b2188586028544b19bb5cd7970a157ff6b1
bexy65 <naimertan65@gmail.com>
2024-05-15 23:52:01 +0300

    remove unneccesery files

--- server/routes/product.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const productController = require("../controller/product");
-
-const router = express.Router();
-
-router.post("/products", productController.createProduct);
-router.patch("/product", productController.updateProduct);
-
-module.exports = router;
